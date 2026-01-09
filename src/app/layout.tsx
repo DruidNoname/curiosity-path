e32e2a0e92193298f3d2c32bc8b94e2deaa950ee
Druid Adamantha <oboro.msu@gmail.com>
2026-01-09 18:12:54 +0300

    app router cache provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next'
 import '../styles/index.css'
 import ClientLayout from './ClientLayout'
-import ErrorBoundary from "@/components/ErrorBoundary";
 import { robotoMono } from '@/assets/fonts/robotoMono';
+import {AppRouterCacheProvider} from "@mui/material-nextjs/v13-appRouter";
 
 export const metadata: Metadata = {
     title: 'Журнал открытой миру',
@@ -16,11 +16,11 @@ const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
     return (
         <html lang="ru" className={robotoMono.variable}>
         <body>
-        <ErrorBoundary componentName={'RootLayout'}>
+        <AppRouterCacheProvider>
             <ClientLayout>
                 {children}
             </ClientLayout>
-        </ErrorBoundary>
+        </AppRouterCacheProvider>
         </body>
         </html>
     )
