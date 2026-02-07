2132b34a18c907645bf03a4f64efab88ab0b1ba8
Druid Adamantha <oboro.msu@gmail.com>
2026-02-08 00:04:29 +0300

    fonts optimization

--- src/assets/fonts/robotoMono.ts
+++ src/assets/fonts/robotoMono.ts
@@ -1,8 +1,34 @@
 // src/fonts/robotoMono.ts
-import localFont from 'next/font/local'
+import localFont from 'next/font/local';
 
 export const robotoMono = localFont({
     src: [
+        {
+            path: './RobotoMono-Light.woff2',
+            weight: '300',
+            style: 'normal',
+        },
+        {
+            path: './RobotoMono-Regular.woff2',
+            weight: '400',
+            style: 'normal',
+        },
+        {
+            path: './RobotoMono-Medium.woff2',
+            weight: '500',
+            style: 'normal',
+        },
+        {
+            path: './RobotoMono-SemiBold.woff2',
+            weight: '600',
+            style: 'normal',
+        },
+        {
+            path: './RobotoMono-Bold.woff2',
+            weight: '700',
+            style: 'normal',
+        },
+        // Fallback в TTF формате для совместимости
         {
             path: './RobotoMono-Light.ttf',
             weight: '300',
@@ -31,4 +57,4 @@ export const robotoMono = localFont({
     ],
     variable: '--font-roboto-mono',
     display: 'swap',
-})
+});
